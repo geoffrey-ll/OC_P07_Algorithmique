9142adbc833d45889c3b21b2bb18e614a982a5b2
Geoffrey Llopis <llopis.geoffrey@laposte.net>
2021-12-30 11:57:10 +0100

    Lecture des actions et leurs données depuis fichier externe.
    Calcul des coût/profit pour chaque combinaisons.

--- bruteforce.py
+++ bruteforce.py
@@ -1,57 +1,75 @@
-import itertools as it
+from itertools import combinations as itcombinations
+from csv import DictReader as csvDictReader
+from csv import DictWriter as csvDictWriter
 
-len_entries = 20
+from pprint import pprint as pp
 
-def create_list_fictive():
-    list_fictive = []
-    for i in range(1, len_entries + 1):
-        list_fictive.append("action" + str(i))
-    return list_fictive
 
-def combinations_with_itertools(list_fictive):
+what_file = "actions-5" # Fichier avec les 5 première actions
+# what_file = "actions" # Fichier avec les 20 actions
+
+list_actions = []
+data_actions = {}
+results_combinations = []
+
+
+def read_actions_file():
+    with open(what_file + ".csv", newline='') as actionsfile:
+        reader = csvDictReader(actionsfile)
+        for row in reader:
+            list_actions.append(row["Action"])
+            data_actions[row["Action"]] = {"Cost": row["cost"], "Profit": row["profit"]}
+
+def find_combinations_possible(list_actions):
     combinations = []
-    for i in range(len(list_fictive) + 1):
-        temp = it.combinations(list_fictive, i)
+    for i in range(len(list_actions) + 1):
+        temp = itcombinations(list_actions, i)
         for elmt in temp:
             combinations.append(elmt)
-    return combinations_with_itertools.__name__, combinations
+    return combinations
 
-def combinations_with_progression(list_fictive):
-    combinations = [()]
-    for action in list_fictive:
-        combinations_with_action = []
-        for combination in combinations:
-            combinations_with_action.append(combination + (action,))
-        combinations += combinations_with_action
-    return combinations_with_progression.__name__, combinations
+def cost_profit(list_actions, combination):
+    result_combination = {}
+    cost = 0
+    profit = 0
+    for action in list_actions:
+        result_combination[action] = "No buy"
 
+    for action in combination:
+        result_combination[action] = "Buy"
+        cost_temp = int(data_actions[action]["Cost"])
+        profit_temp = float(data_actions[action]["Profit"])
+        cost += cost_temp
+        profit += cost_temp * profit_temp
+    result_combination["Cost"] = cost
+    result_combination["Profit"] = format(profit, ".2f")
 
-
-def results_combinations(result):
-    name_function = result[0]
-    combinations_find = result[1]
-    if len(combinations_find) <= 20:
-        print(combinations_find)
-        print("Soit un total de {} combinaisons trouvées avec la "
-              "fonction {}".format(len(combinations_find), name_function)
-              )
-    else:
-        print("Avec la fonction {}, {} combinaisons ont "
-              "été trouvées !".format(name_function, len(combinations_find))
-              )
-
-
-
+    return results_combinations.append(result_combination)
 
 def main():
-    list_fictive = create_list_fictive()
-    results = []
-    results.append(combinations_with_itertools(list_fictive))
-    results.append(combinations_with_progression(list_fictive))
+    read_actions_file()
+    combinations = find_combinations_possible(list_actions)
+    print("Calcul des coût/profit en cours")
+    for combination in combinations:
+        cost_profit(list_actions, combination)
+    print("Calculs terminés")
 
-    for result in results:
-        results_combinations(result)
+    print_result()
 
 
+def print_result():
+    if len(results_combinations) <= 50:
+        for result in results_combinations:
+            print(result)
+    else:
+        for result in results_combinations[0:5]:
+            pp(result)
+        for result in results_combinations[-5:]:
+            pp(result)
+
+        print(""
+              "Les 10 premiers et 10 derniers, d'un total de {} résultats"
+              .format(len(results_combinations))
+              )
 
 main()
